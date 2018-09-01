195f891c10cf2156d409d67c05d60a5ba6c507e3
Saiday <feocms@gmail.com>
2018-09-01 16:02:42 +0800

    tickets

--- aristotle/models.py
+++ aristotle/models.py
@@ -7,7 +7,7 @@ from aristotle import settings
 
 class ViolationCategoryMeta(EnumMeta):
     def __iter__(self):
-        return ((tag, tag.value) for tag in super().__iter__())
+        return ((tag.name, tag.value) for tag in super().__iter__())
 
 
 class ViolationCategory(Enum, metaclass=ViolationCategoryMeta):
@@ -18,4 +18,5 @@ class Ticket(models.Model):
     violation = models.CharField(max_length=10, choices=ViolationCategory)
     plate_number = models.CharField(max_length=10)
     location = models.CharField(max_length=256)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
--- users/admin.py
+++ users/admin.py
@@ -24,8 +24,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
-        ),
+            'fields': ('email', 'password1', 'password2')}),
     )
     form = CustomUserChangeForm
     add_form = CustomUserCreationForm
